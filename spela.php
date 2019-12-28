deee16c22b772e6425ac0472e0f128bc1c28a227
jlivingstonsg <magnus.ivarsson@gmail.com>
2019-12-28 12:01:38 +0100

    Update spela.php
    Only work in firefox

--- spela.php
+++ spela.php
@@ -1,20 +1,15 @@
 <?php
-
 if(!isset($_COOKIE['queue'])) { header('Location: index.php?check_cookie'); exit; }
-
-
 //list($ip, $port) = explode(':', $_SERVER['SERVER_NAME']);
 //if(!$port) $ip='192.168.1.223';
 //$kamera_adr = "http://$ip:8088";
 
-$RoboCam_adr = "http://".trim(file_get_contents('status/robot_ip')).":8088/";
 $kamera_adr = "http://".trim(file_get_contents('status/robot_ip')).":8088/cam";
-
 function webcamklient($typ) {
 	global $kamera_adr;
 	switch($typ) {
 		case 'mjpg':
-			echo '<img width="100%" height="100%" src="',$kamera_adr,'"/>';
+			echo '<img width="640" height="480" src="',$kamera_adr,'"/>';
 			break;
 	//	case 'java':
 	//		echo '<applet code="com.charliemouse.cambozola.Viewer" archive="',$kamera_adr,'/cambozola.jar" width="640" height="480">',
@@ -32,37 +27,19 @@ function webcamklient($typ) {
 			echo '<img width="640" height="480" src="',$kamera_adr,'"/>';
 	}
 }
-
 if(isset($_GET['webcamklient'])) { webcamklient($_GET['webcamklient']); exit; }
-
-
 //session_start(); //Använd sessions
 header('Cache-Control: no-cache');
-
 $speltid = file_get_contents('status/speltid');
 $queue = explode("\n", file_get_contents('status/queue')); //Läs in köfilen
-
-
-
 $iparray = (array) json_decode(file_get_contents('status/ip_block'));
 $ip_block = isset($iparray[$_SERVER["REMOTE_ADDR"]]) && $iparray[$_SERVER["REMOTE_ADDR"]]>(time()-3600);
 	
-
-
 if($ip_block) {
 	$fh = fopen('status/återbesökare', 'a');
 	fwrite($fh, $_COOKIE["queue"]."\n");
 	fclose($fh);
 }
-
-
-
-
-
-
-
-
-
 if(!$queue[1]) {//Om aktuell spelare inte aktiverat sig
  if(isset($queue[2]) && $queue[2]==$_COOKIE['queue']) {
 	$queue[1]=true;
@@ -70,7 +47,6 @@ if(!$queue[1]) {//Om aktuell spelare inte aktiverat sig
  }
  elseif($queue[0] < time()+$speltid-5) $queue[0] = 0; //Om spelaren som står på tur inte varit aktiv inom 5 sek så kastas hen ut i nedanstående
 }
-
 if($queue[0] <= time()) { //Uppdatera kön ifall tiden har förflutit
 	unset($queue[2]);
 	$queue = array_values($queue);
@@ -78,18 +54,12 @@ if($queue[0] <= time()) { //Uppdatera kön ifall tiden har förflutit
 	$queue[0] = time()+$speltid;
 	file_put_contents('status/queue', implode("\n", $queue));
 }
-
 $queue_pos = array_search($_COOKIE['queue'], $queue, true); //2=Aktuell spelare, 3=nästkommande spelare ..., false=Inte i kön
-
 if($queue_pos != 2) {
 	header('Location: queue.php');
 	exit;
 }
-
-
 header('Content-Type: text/html; charset=utf8');
-
-
 // Sniffa browser:
  preg_match('/Chrome\/(\d+\.\d+)/', $_SERVER['HTTP_USER_AGENT'], $d); $chrome = (float) $d[1];
  preg_match('/Opera[\/\s](\d+\.\d+)/', $_SERVER['HTTP_USER_AGENT'], $d); $opera = (float) $d[1];
@@ -99,33 +69,22 @@ header('Content-Type: text/html; charset=utf8');
  preg_match('/Konqueror\/(\d+\.\d+)/', $_SERVER['HTTP_USER_AGENT'], $d); $konqueror = (float) $d[1];
  preg_match('/AppleWebKit\/(\d+(\.\d+)?)/', $_SERVER['HTTP_USER_AGENT'], $d); $awk = (float) $d[1]; //bättre än safari enligt flera källor
  preg_match('/KHTML\/(\d+(\.\d+)?)/', $_SERVER['HTTP_USER_AGENT'], $d); $khtml = (float) $d[1];
-
-
-
-
-
-
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="sv" lang="sv">
 
 
-<head>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>GameReality</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	
    <meta name="description" content="GameReality. En spelvärld med nya händelser varje dag. Tryck på knapparna W,A,S,D och pilarna höger/vänster och styr roboten." />
 	<meta name="generator" content="Geany 0.18" />
 	<link href="style.css" media="screen" rel="stylesheet" type="text/css" /> 
 	<script type="text/JavaScript">/*<![CDATA[*/
-
 var errT = false;	
-
-
-
 function restart_webcam() {
 	var img = document.getElementById('webcam').getElementsByTagName('img');
 	img[0].src="<?php echo $kamera_adr; ?>/?action=snapshot&f="+imgCnt++;
 }
-
 function body_key(e) {
 	if(!e) e = window.event;
 	//alertarea('keyCode: '+e.keyCode+', which: '+e.which, 2000);
@@ -139,7 +98,6 @@ function body_key(e) {
 		
 	}
 }
-
 	
   /*]]>*/</script>
 </head>
@@ -148,107 +106,49 @@ function body_key(e) {
 <div id="knappanel"></div>
 
 
-<!--
-<?php
-//echo '<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fvalslaget.se+&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>';
-
-//echo '<div id="fb" xstyle="float:right; width: 292px; margin-left:20px;">';
-//echo '<fb:comments numposts="5" width="292"></fb:comments>';
-//echo '<img id="statusbild" src="images/status.jpg" alt="aktuell status" title="aktuell status" style="width:292px;"/>';
-//echo '<iframe src="http://www.facebook.com/plugins/likebox.php?id=125097487508475&amp;width=292&amp;connections=10&amp;stream=true&amp;header=true&amp;height=587" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:587px;" allowTransparency="true"></iframe>';
-//echo '</div>';
-?>
-
--->
-
 
 <div id="top">
-    
-<!--	
-<h1><a href="http://www.gamereality.se/" target="_top">GameReality 2019</a></h1>
-
-
-
-<p>En spelvärld med nya händelser varje dag. Tryck på knapparna W,A,S,D och pilar höger/vänster och kontrollera roboten.
-</p>
--->
+<p>Push keys W,A,S,D and arrows to control the robot. </p>
 </div>
-
-
-
-
-<!--
-<div style=" color:black;position:absolute; top: 0; right: 0;border: outset 1px; background: white;padding:7px;text-align:right;">
-	Problem med bilden?<br/> testa någon av dessa:<br/>
-<input type="image" src="images/MJPG.png" onclick="setWebcam('mjpg');" title="Visa kamerabild som MJPG-bild"/>
-<input type="image" src="images/Duke.png" onclick="setWebcam('java');" title="Använd Javabaserad kameravisning"/>
-<input type="image" src="images/js.png" onclick="setWebcam('javascript');" title="Använd Javascriptbaserad kameravisning"/>
-</div>
-
-
--->
-
-
-<iframe 
-	style="width:100%;height:75%;border:none;"
-	src="<?php echo htmlspecialchars($RoboCam_adr); ?>"
->
-</iframe>
-
-
-
-<?php if(false): ?>
-
-	<div style="position:absolute; top: 0; left: 0;border: outset 1px; background: #ffff77;padding:7px;text-align:left;">
-		Välkommen! Din ip-adress är (<?php echo $_SERVER["REMOTE_ADDR"]; ?>) 
-	</div>
-
-<?php endif; ?>
-
 
 <div id="fb-root"></div>
 
 <div id="scenen">
-
 <div id="webcamcontainer">
+
 <div id="imgtopleft" onclick="send_cmd('go_4')" title="Längst fram till vänster"></div>
 <div id="imgtopright" onclick="send_cmd('go_5')" title="Längst fram till höger"></div>
 <div id="imgleft" onclick="send_cmd('go_2')" title="Till vänster"></div>
 <div id="imgright" onclick="send_cmd('go_3')" title="Till höger"></div>
-
 <div id="imgback" onclick="send_cmd('go_back')" title="tillbaka"></div>
 <div id="alertarea"></div>
-<div id="webcam">
 
+<div id="webcam">
 <?php webcamklient($gecko>0? 'mjpg': 'javascript'); ?>
 </div>
+
 
   <div id="progress_frame">
    <div id="progress"></div>
    <input id="progress_info" value="Var god vänta..."/>
   </div>
   
+
 <div id="arrowbox">
 	<div id="goup" onclick="send_cmd('go_fwd');"></div>
 	<div id="godown" onclick="send_cmd('go_back');"></div>
 	<div id="goleft" onclick="send_cmd('go_left');"></div>
 	<div id="goright" onclick="send_cmd('go_right');"></div>
 </div>  
-  
-
-  
-<div id="msg1" style="position:absolute; top:485px;height:70px;width:416px;text-align:left;padding:4px;">Var god dröj, robot startas...
-</div>
 
 
-  
-  <button style="float:left;clear:right;margin-bottom:5px;" onclick="send_cmd('close'); window.location.href='queue.php?exit'" style="border-color:red;">Avbryt</button>
+
+<button style="float:left;clear:right;margin-bottom:5px;" onclick="send_cmd('close'); window.location.href='queue.php?exit'" style="border-color:red;">Avbryt</button>
   
   
 <!-- 
   <div id="luckarea" xonclick="luckoppning(event.target)">
 <?php
-
 	$imorgon = min(25, date('d')+1);
 	$imorgon = 25;
 	for($i=1; $i<$imorgon; $i++) {
@@ -273,35 +173,24 @@ function body_key(e) {
 
 
   <script type="text/JavaScript">/*<![CDATA[*/
-
   var preloadImg = new Image(100,25); 
   preloadImg.src="images/glutta.png"; 
-
-
 function on_resize() { //Se till att ytan ovanför bilden inte visas i onödan
 	var h = Math.min(120, Math.max(0, window.innerHeight-640))+'px';
 	if(h.match(/^\d+px$/)) document.getElementById('top').style.height = h;
 }
-
 window.onresize = on_resize;
 on_resize();
-
-
-
-
  
 var start_time = <?php echo $queue[0]-time(); ?>; //Den totala speltiden när sidan laddades
 var progress = document.getElementById("progress");
 var progress_info = document.getElementById("progress_info");
 progress.style.right = '0'; 
 progress.style.backgroundPosition = 'right'; 
-
 var time = new Date(); //Aktuell tid
 var time_end  = <?php echo $queue[0]-time(); ?>*1000+time.getTime(); //Beräknad tid när spelandet avbryts
 var time_check = time.getTime()+1000; //Nästa gång tiden kollas mot servern
 //var infoHttpObj = new XMLHttpRequest();
-
-
 function server_response() { //Denna funktion anropas var gång servern svarar
 	//alertarea('x', 150);
 	if (this.readyState==4){
@@ -315,12 +204,8 @@ function server_response() { //Denna funktion anropas var gång servern svarar
 	 else if(this.responseText.length) alertarea(this.responseText, 10000);
 	}
 }
-
 //var aktiva_cmd = {};
 //aktiva_cmd['a'] = '123';
-
-
-
 function infoTimer() {
 	//alertarea('x', 150);
 	time = new Date();
@@ -347,11 +232,7 @@ function infoTimer() {
 //		alertarea(http.responseText, 150);
 //	}
 }
-
 infoTimer(); //Starta loopen
-
-
-
 function updateInfo(tid) {
 	if(tid > 0) {
 	progress.style.width = Math.round(0.123*tid/start_time+1)+'px';
@@ -365,28 +246,18 @@ function updateInfo(tid) {
 	}
 	//else window.location.href="tackochhej.php";
 }
-
 // ==================== STYRSYSTEMET: ========================
-
 function httpObj() {
 	return new XMLHttpRequest();
 }
-
 var imgCnt = 0; //Bildräknare för unika bilder
-
 var servoLeft = 62;
-
-
-
 var status = document.getElementById("infoarea");
-
 function debug(obj) {
 	var t = "";
 	for(var x in obj) t += x+', ';
 	alert(t);
 }
-
-
 function send_cmd(cmd) {
 	var http = new XMLHttpRequest();
 	http.t = new Date().getTime();
@@ -463,11 +334,7 @@ function send_cmd(cmd) {
 	http.send(null);
 	//aktiva_cmd[http.cmd] = http;
 }
-
-
 send_cmd('start'); //Försätt teatern i startläge
-
-
 function alertarea(msg, tid) {
 	var aa = document.getElementById('alertarea');
 	aa.style.display = 'block';
@@ -476,11 +343,6 @@ function alertarea(msg, tid) {
 		document.getElementById('alertarea').style.display = 'none';
 	}, tid);
 }
-
-
-
-
-
 function screenshot() { //Inte i bruk för närvarande, men en trevlig funktion var det. Kanske att återinföra igen.
 	var http = new XMLHttpRequest();
 	http.open('GET', 'cmd.php/snapshot', true);
@@ -490,17 +352,12 @@ function screenshot() { //Inte i bruk för närvarande, men en trevlig funktion 
 	}
 	http.send(null);
 }
-
-
 function setWebcam(value) {
 	var http = new XMLHttpRequest();
 	http.open('GET', '?webcamklient='+encodeURIComponent(value), false);
 	http.send(null);
 	document.getElementById('webcam').innerHTML = http.responseText;
 }
-
-
-
 /*
 //Lite facebook-kod:
 window.fbAsyncInit = function() {FB.init({appId: '128949067127332', status: true, cookie: true, xfbml: true}); };
@@ -512,12 +369,7 @@ window.fbAsyncInit = function() {FB.init({appId: '128949067127332', status: true
         e.async = true;
         document.getElementById('fb-root').appendChild(e);
       }());
-
 */
-
-
-
-
 function luckoppning(lucka) {
 	var t = lucka.textContent === undefined ? lucka.innerText: lucka.textContent;
 //	alert(t);
@@ -529,8 +381,6 @@ function luckoppning(lucka) {
 	lucka.style.color = 'blue';
 //	setTimeout(function() {lucka.style.backgroundImage = null;}, 20000); //Automatisk luckstängning
 }
-
-
 function mellanklickrutor(aktiv) {
 	try {
 		document.getElementById('imgleft').style.display = aktiv? null: 'none';
@@ -544,25 +394,13 @@ function mellanklickrutor(aktiv) {
 }
 	
 	
-
 function debug(obj) {
 	var t = '';
 	for(var x in obj) t += x+', ';
 	alertarea(t, 200);
 }
-
-
-
-
-
 //document.getElementsByTagName('body')[0].setAttribute("onkeydown",'body_key(event)');
 window.focus();
-
-
-
-
-
-
   /*]]>*/</script>
  
 
@@ -570,4 +408,3 @@ window.focus();
 
  </body>
 </html>
-
